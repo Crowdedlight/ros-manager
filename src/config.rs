ea433f6eef25ea73899022dba768eb6d2595f2ab
Frederik Mazur Andersen <crowdedhd@gmail.com>
2022-11-19 19:57:24 +0100

    fixed so it can be packed to appimage, and added location for config file

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,7 @@ use std::fs;
 use std::io::prelude::*;
 use serde::{Serialize, Deserialize, serde_if_integer128};
 use serde;
+use std::env;
 
 #[derive(Serialize, Debug, Deserialize, Clone)]
 pub struct Cfg {
@@ -11,11 +12,11 @@ pub struct Cfg {
 
 impl Cfg {
     pub fn init(&mut self) -> Result<(), i32> {
-        // let mut w = CONFIG.write().unwrap();
+        // get config path
+        let conf_file = self.getPathToConfig();
 
-        /* read the config file */
         let _lcfg: Cfg =
-            toml::from_slice(&fs::read("./src/settings.toml").unwrap()).unwrap();
+            toml::from_slice(&fs::read(conf_file).unwrap()).unwrap();
 
         self.ws_root_path = _lcfg.ws_root_path;
 
@@ -23,8 +24,26 @@ impl Cfg {
     }
     pub fn save(&self) -> Result<(), i32> {
         // save current state to file
+
+        let conf_file = self.getPathToConfig();
         // let c = CONFIG.read().unwrap();
-        &fs::write("./src/settings.toml", toml::to_string(&self).unwrap()).unwrap();
+        &fs::write(conf_file, toml::to_string(&self).unwrap()).unwrap();
         Ok(())
+    }
+
+    fn getPathToConfig(&self) -> String {
+        // get debug or release path
+        let mut config_path = "".to_string();
+        match env::var("XDG_CONFIG_HOME") {
+            Ok(v) => config_path = v.clone(),
+            Err(e) => config_path = "./config".to_string()
+        }
+
+        // add config filename
+        config_path += "/settings.toml";
+
+        /* read the config file */
+        println!("{}",config_path);
+        return config_path
     }
 }
